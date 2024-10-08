340e1b213c7ebfc5c5be9847a45bf47f34377c18
meiaus2024 <165844429+meiaus2024@users.noreply.github.com>
2024-10-07 20:17:35 -0500

    Update IndexServlet.java

--- src/main/java/libertymq/IndexServlet.java
+++ src/main/java/libertymq/IndexServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "IndexServlet", urlPatterns = "/libertymq")
+@WebServlet(name = "IndexServlet", urlPatterns = "/")
 @ServletSecurity(value = @HttpConstraint(rolesAllowed = {"users"},
         transportGuarantee = ServletSecurity.TransportGuarantee.CONFIDENTIAL))
 public class IndexServlet extends HttpServlet {
